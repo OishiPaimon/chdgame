ae141276c8a8e591b1b4b731d70020153a761835
yyx <ylnx0321@163.com>
2025-04-28 23:14:35 +0800

    1、增加mana进度条

--- Script/Class/Status.gd
+++ Script/Class/Status.gd
@@ -2,8 +2,12 @@ class_name Status
 extends Node
 
 signal health_changed
+signal mp_changed
+signal exp_changed
 
 @export var max_health:int=3
+@export var max_mp:int=100
+@export var max_exp:int=100
 
 @onready var health: int = max_health:
 	set(v):
@@ -12,3 +16,26 @@ signal health_changed
 			return
 		health=v
 		health_changed.emit()
+
+@onready var mp: int = max_mp:
+	set(v):
+		v=clampi(v,0,max_mp)
+		if mp==v:
+			return
+		mp=v
+		mp_changed.emit()
+
+@onready var exp: int = 0:
+	set(v):
+		v=clampi(v,0,max_exp)
+		if exp==v:
+			return
+		exp=v
+		exp_changed.emit()
+
+func add_exp(amount: int):
+	exp += amount
+	if exp >= max_exp:
+		exp = 0
+		# 升级逻辑
+		
--- Script/UI/player_status_panel.gd
+++ Script/UI/player_status_panel.gd
@@ -4,14 +4,25 @@ extends HBoxContainer
 
 @onready var health_bar: TextureProgressBar = $VBoxContainer/HealthBar
 @onready var real_heath_bar: TextureProgressBar = $VBoxContainer/HealthBar/RealHeathBar
+@onready var mana_bar: TextureProgressBar = $VBoxContainer/ManaBar
+@onready var real_mana_bar: TextureProgressBar = $VBoxContainer/ManaBar/RealManaBar
 
 func init():
 	if status:
 		status.health_changed.connect(update_health)
+		status.mp_changed.connect(update_mp)
+		
 		update_health()
+		update_mp()
 
 func update_health():
 	var percentage := status.health / float(status.max_health)
 	health_bar.value = percentage
 	real_heath_bar.value = percentage
 	create_tween().tween_property(health_bar, "value", percentage, 0.3)
+
+func update_mp():
+	var percentage := status.mp / float(status.max_mp)
+	mana_bar.value = percentage
+	real_mana_bar.value = percentage
+	create_tween().tween_property(mana_bar, "value", percentage, 0.3)
